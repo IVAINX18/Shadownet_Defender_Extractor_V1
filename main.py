06d5704eba88f2c16331ecf108a35f6d3db71fc0
IVAINX18 <ivainxsanchez18@gmail.com.com>
2026-02-17 13:41:43 -0500

    chore: Update default scanned sample from procexp.exe to procexp64.exe.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from core.scanner import scan_file
 from rich import print
 
-result = scan_file("samples/procexp.exe")
+result = scan_file("samples/procexp64.exe")
 
 print("[bold cyan]ShadowNet Defender Scan Result[/bold cyan]")
 print(result)
